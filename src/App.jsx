5791b3e7369a004d3988a44b7224510fac1fd142
Justine Gallien <justinegallien@gmail.com>
2025-07-31 21:18:26 -0500

    modified files

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,39 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Employee } from "./page/Employee.jsx";
 import { Home } from "./page/Home.jsx";
 import { EditEmployee } from "./page/EditEmployee.jsx";
 import { Tasks } from "./page/Tasks.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Menu } from "./components/Menu.jsx";
 
 function App() {
+  const [isLogIn, setLogIn] = useState(false);
+
+  const validateToken = () => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setLogIn(true);
+    }
+  };
+
+  useEffect(() => {
+    validateToken();
+  }, []);
+
   return (
     <>
       <BrowserRouter>
+        {isLogIn && <Menu auth={setLogIn} />}
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/employee" element={<Employee />} />
-          <Route path="/editEmployee/:employee_id" element={<EditEmployee />} />
-          <Route path="/task/:employee_id" element={<Tasks />} />
+          <Route path="/" element={<Home auth={setLogIn} />} />
+          {isLogIn && <Route path="/employee" element={<Employee />} />}
+          {isLogIn && (
+            <Route
+              path="/editEmployee/:employee_id"
+              element={<EditEmployee />}
+            />
+          )}
+          {isLogIn && <Route path="/task/:employee_id" element={<Tasks />} />}
         </Routes>
       </BrowserRouter>
     </>
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,5 +1,83 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export const Home = () => {
-  return <h1>Home</h1>;
+const baseUrl = import.meta.env.VITE_BASE_URL;
+const endPoint = "user/auth";
+
+export const Home = ({ auth }) => {
+  const navigate = useNavigate();
+
+  const [resultLogIn, setResultLogIn] = useState("");
+
+  const [body, setBody] = useState({
+    email: "",
+    password: "",
+  });
+
+  const formHandler = (event) => {
+    const temp = {
+      email: body.email,
+      password: body.password,
+    };
+    temp[event.target.name] = event.target.value;
+    setBody(temp);
+  };
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    const url = `${baseUrl}${endPoint}`;
+
+    const result = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (result.ok) {
+      const data = await result.json();
+      localStorage.setItem("token", data.token);
+      auth(true);
+      navigate("/person");
+    } else {
+      setResultLogIn("Invalid Credentials");
+    }
+  };
+
+  return (
+    <>
+      <div className="container vh-100 d-flex align-items-center justify-content-center">
+        <div className="card p-4" style={{ maxWidth: "80%", width: "100%" }}>
+          <h3 className="text-center mb-2">Log In</h3>
+          <form onSubmit={submitHandler}>
+            <div className="mb-3">
+              <label className="form-label">Email</label>
+              <input
+                name="email"
+                onChange={formHandler}
+                type="email"
+                className="form-control"
+              />
+            </div>
+
+            <div className="mb-3">
+              <label className="form-label">Password</label>
+              <input
+                name="password"
+                onChange={formHandler}
+                type="password"
+                className="form-control"
+              />
+            </div>
+
+            <p>{resultLogIn}</p>
+
+            <button className="btn btn-primary w-100">Log In</button>
+          </form>
+          <button className="btn btn-secondary mt-2 w-100">Register</button>
+        </div>
+      </div>
+    </>
+  );
 };
